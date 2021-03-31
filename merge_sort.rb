0d6262f49ad2f7755f4e24e99a5b21b5accb975f
Guillermo <guillermoc93@protonmail.com>
2021-03-31 07:53:09 -0500

    finished merge_sort algorithm

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,10 +1,30 @@
 def merge_sort(array)
-  len = array.length - 1
-  half = (array.length - 1) / 2
-  first_half = array[0..half]
-  second_half = array[half + 1..len]
+  if array.length < 2
+    array
+  else
+    len = array.length
+    half = (array.length / 2)
 
-  
+    left = array[0...half]
+    right = array[half..len]
+    merge(merge_sort(left), merge_sort(right))
+  end
 end
 
-merge_sort([1, 2, 3, 4, 5, 6, 7])
+def merge(left, right)
+  merged_array = []
+
+  while left.any? && right.any?
+    if left[0] < right[0]
+      merged_array << left.shift
+    elsif right[0] < left[0]
+      merged_array << right.shift
+    end
+  end
+
+  merged_array << left.shift until left.empty?
+  merged_array << right.shift until right.empty?
+  merged_array
+end
+
+p merge_sort([14, 12, 3, 5, 63, 11, 23, 88, 89, 2])
